09c00206601503a0b950a4e6b9fc780589d2cbcd
Sahir Karani <sahir.karani@gmail.com>
2017-03-11 17:39:59 -0500

    Added comments to the nexmo API and cleaned up the code

--- nexmoAPI.py
+++ nexmoAPI.py
@@ -4,16 +4,15 @@ import json
 import time
 from secret import *
 
-print API_KEY
-print API_SECRET
-print FROM_NUMBER
 
 def sendText(number, text):
 	'''This function sends out a single text message to the provided number as a string it also 
-	checks if the number starts with 1 since this is planned to be used primarly in the US.'''
+	checks if the number starts with 1 since this is planned to be used primarly in the US. 
+	Function returns True or False if the message was delivered successfully.'''
 	if number[0] != '1': #check for starting with 1
 		number = "1" + number #if not add 1
 	
+	#create the json payload
 	params = {
 		'api_key': API_KEY,
 		'api_secret': API_SECRET,
@@ -22,10 +21,12 @@ def sendText(number, text):
 		'text': text
 	}
 	
+	
 	url = 'https://rest.nexmo.com/sms/json?' + urllib.urlencode(params)
 	print url
 	request = urllib2.Request(url)
 	request.add_header('Accept', 'application/json')
+	#send/retreive the requests
 	response = urllib2.urlopen(request)
 	
 	if response.code == 200 :
@@ -34,14 +35,15 @@ def sendText(number, text):
 		decoded_response = json.loads(data.decode('utf-8'))
 		# Check if your messages are succesful
 		messages = decoded_response["messages"]
-		print '--------------'
-		print messages
 		for message in messages:
 			if message["status"] == "0":
 				print "success"
+			else:
+				return False
+		return True
+		
 	else :
 		#Check the errors
 		print "unexpected http {code} response from nexmo api". response.code
 		print response.code
-
-
+		return False
